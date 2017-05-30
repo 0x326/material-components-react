e3248fe10e572bf3a8f24c5eb3bf6018246cdfa5
John Meyer <0x326@users.noreply.github.com>
2017-05-30 14:40:26 -0400

    Upgrade to JSX if-else syntax

--- src/ListItem.js
+++ src/ListItem.js
@@ -29,11 +29,11 @@ export default class ListItem extends PureComponent {
       <li className="mdc-list-item"
         id={this.props.id}>
         {this.startDetail}
-        {this.props.secondaryText.length === 0 && this.props.text}
-        {this.props.secondaryText.length !== 0 && <span className="mdc-list-item__text">
-          {this.props.text}
-          <span className="mdc-list-item__text__secondary">{this.props.secondaryText}</span>
-        </span>}
+        {this.props.secondaryText.length === 0 ? this.props.text :
+          <span className="mdc-list-item__text">
+            {this.props.text}
+            <span className="mdc-list-item__text__secondary">{this.props.secondaryText}</span>
+          </span>}
         {this.endDetail}
       </li>
     )
